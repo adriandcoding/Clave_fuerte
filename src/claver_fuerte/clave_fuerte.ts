87036ca2ccc589ee2a0d2b5d8cd09eb6540f417c
adriandcoding <theamr91@gmail.com>
2024-06-06 13:58:29 +0200

    mejorando el testing

--- src/claver_fuerte/clave_fuerte.ts
+++ src/claver_fuerte/clave_fuerte.ts
@@ -1,23 +1,37 @@
+import {
+    tieneCaracteresEspeciales,
+    tieneLongitudMinima,
+    tieneMayusculasYMinusculas,
+    tieneNumeros,
+    tieneNombreUsuario,
+    tienePalabrasComunes,
+} from "./clave_fuerte-helper"
 const validarClave = (
     nombreUsuario: string,
     clave: string,
     commonPasswords: string[]
 ): ValidacionClave => {
-    const validaciones: ValidacionClave[] = [
-        tieneMayusculasYMinusculas(clave),
-        tieneNumeros(clave),
+    const validaciones = [
         tieneCaracteresEspeciales(clave),
         tieneLongitudMinima(clave),
+        tieneMayusculasYMinusculas(clave),
+        tieneNumeros(clave),
         tieneNombreUsuario(nombreUsuario, clave),
         tienePalabrasComunes(clave, commonPasswords),
     ];
-
-    const resultadoValidacion = validaciones.find(validacion =>!validacion.esValida);
-    if (resultadoValidacion) {
-        return resultadoValidacion;
-    } else {
+    const validacionesValidas = validaciones.filter(validacion => validacion.esValida);
+    if (validacionesValidas.length === validaciones.length) {
         return { esValida: true };
+    } else {
+        return {
+            esValida: false,
+            error: validacionesValidas[0].error,
+        };
     }
 
 
-  }
+    
+
+}
+
+
